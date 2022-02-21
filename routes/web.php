8142f67be99797673472c8730b46dce7fededf15
Hossam Ali <64331041+HossamAli999@users.noreply.github.com>
2022-02-21 17:30:59 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
