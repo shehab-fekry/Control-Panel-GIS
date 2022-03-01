94780fdb9f443ffdf3b4b3f112927d3cf3a07a4f
AhmedElgizawyy <98716647+AhmedElgizawyy@users.noreply.github.com>
2022-03-01 02:41:18 +0200

    c

--- app/Http/Controllers/API/ChildController.php
+++ app/Http/Controllers/API/ChildController.php
@@ -100,8 +100,7 @@ class ChildController extends BaseController
         elseif($father->trip_id==null){
 
 
-
-            return $this->sendError('please validate errors','your account  not assigned to any trip yet please contact with one of school admins');
+            return $this->sendError('please validate errors','your account  not assigned to trip yet please contact with one of school admins');
         }elseif(Auth::guard('api-fathers')->id()!==$child->father_id){
             return $this->sendError('please validate errors',"you are not authorized to do this action");
         }elseif($child->status==false){
