b9e536ff473bcc5bd255edc3c68c7c1bde272402
AhmedElgizawyy <98716647+AhmedElgizawyy@users.noreply.github.com>
2022-06-08 14:58:14 +0200

    c

---
+++ app/Events/tripNotification.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class tripNotification implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+    public $trip_id;
+    public $message;
+    public function __construct($data)
+    {
+        $this->trip_id=$data['trip_id'];
+        $this->message=$data['message'];
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new Channel('trip.'.$this->trip_id);
+    }
+    public function broadcastAs()
+    {
+        return 'Gizawy';
+    }
+
+}
