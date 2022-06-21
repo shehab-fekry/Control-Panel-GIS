3bd65bd7e377545828dfc2f278bea340021a80c9
Shehab Fekry <shehabeddin99@gmail.com>
2022-06-21 15:56:40 +0200

    bug fixed

--- public/js/tracking.js
+++ public/js/tracking.js
@@ -59,7 +59,7 @@ const initTrack = (tripIndex) => {
         state.trackingMap = new mapboxgl.Map({
             container: document.getElementById('map'),
             style: 'mapbox://styles/shehab-fekry/cl0e4k50n002p14si2n2ctxy9',
-            center: [schoolLocation],
+            center: schoolLocation,
             zoom: 13
             });
 
